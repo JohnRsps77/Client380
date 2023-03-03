43f6b2940ceacdaab9bf0980cd2850058493d03c
John <Jeanhanna1234567@gmail.com>
2023-03-02 17:41:02 -0800

    javafx and refactor

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.client {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.client to javafx.fxml;
+    exports com.client;
+    exports com.client.controllers;
+    opens com.client.controllers to javafx.fxml;
+}
