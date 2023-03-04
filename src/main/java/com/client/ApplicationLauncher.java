c37cf5d2b5f48aee5dc09b8c196615ce924c131f
Thien Vong <thien.vong.892@my.csun.edu>
2023-03-03 17:28:43 -0800

    Login screen UI prototype, SceneManager for switch scene, re-arrange file

--- src/main/java/com/client/ApplicationLauncher.java
+++ src/main/java/com/client/ApplicationLauncher.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class ApplicationLauncher extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ApplicationLauncher.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("views/login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/client/controllers/LoginController.java
+++ src/main/java/com/client/controllers/LoginController.java
@@ -1,14 +1,35 @@
 package com.client.controllers;
 
+import com.client.SceneManager;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
 
+    public final SceneManager sceneManager = new SceneManager();
+
+
+    @FXML
+    public void switchToMainScene(ActionEvent event) throws IOException {
+        SceneManager.SceneType sceneType = SceneManager.SceneType.MAIN_SCENE;
+        sceneManager.switchScene(event, sceneType);
+    }
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+
 }
