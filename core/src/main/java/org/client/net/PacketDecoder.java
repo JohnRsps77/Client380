53b2fde6fadcf2472abc3f7dd74801dfb7d3f04a
John <Jeanhanna1234567@gmail.com>
2023-03-09 20:32:08 -0800

    remove last of libgdx

--- core/src/main/java/org/client/net/PacketDecoder.java
+++
@@ -1,8 +0,0 @@
-package org.client.net;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-public interface PacketDecoder {
-    void decode(InputStream inputStream) throws IOException;
-}
