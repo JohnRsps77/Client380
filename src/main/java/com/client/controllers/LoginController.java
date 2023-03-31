0de5cb8520283e7243d773a2499a36bf327da9ba
Thien Vong <thien.vong.892@my.csun.edu>
2023-03-31 14:19:53 -0700

    Login-view UI done

--- src/main/java/com/client/controllers/LoginController.java
+++ src/main/java/com/client/controllers/LoginController.java
@@ -4,8 +4,12 @@ import com.client.Client;
 import com.client.managers.SceneManager;
 import com.client.model.SceneType;
 import javafx.fxml.FXML;
+import javafx.scene.input.InputEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,7 +17,16 @@ import java.util.ResourceBundle;
 public class LoginController implements Controller {
 
     private final SceneManager sceneManager = Client.getInstance().getSceneManager();
-
+    private boolean isLogin = false;
+    @FXML
+    public void LoginAction() throws IOException
+    {
+        isLogin = true;
+        if(isLogin == true)
+        {
+            sceneManager.switchScene(SceneType.MAIN_SCENE);
+        }
+    }
     @FXML
     public void switchToMainScene() throws IOException {
         sceneManager.switchScene(SceneType.MAIN_SCENE);
@@ -22,6 +35,21 @@ public class LoginController implements Controller {
     @FXML
     public void switchToSignUpScene() throws IOException {
         sceneManager.switchScene(SceneType.SIGNUP_SCENE);
+    }
+
+    @FXML
+    Button closeButton = new Button();
+    @FXML
+    Button minimizeButton = new Button();
+    @FXML
+    public void closeAction() throws IOException
+    {
+        sceneManager.closeWindow(closeButton);
+    }
+    @FXML
+    public void minimizeAction() throws IOException
+    {
+        sceneManager.minimizeWindow(closeButton);
     }
 
     @Override
@@ -33,8 +61,11 @@ public class LoginController implements Controller {
         System.out.println("Clicked");
     }
 
+
+
     @Override
-    public void clear() {
+    public void clear()
+    {
 
     }
 }
--- src/main/java/com/client/managers/SceneManager.java
+++ src/main/java/com/client/managers/SceneManager.java
@@ -3,11 +3,14 @@ package com.client.managers;
 import com.client.Client;
 import com.client.controllers.Controller;
 import com.client.model.SceneType;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
+import java.awt.*;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Objects;
@@ -33,6 +36,16 @@ public class SceneManager {
 
         stage.setWidth(sceneType.getWidth());
         stage.setHeight(sceneType.getHeight());
+    }
+
+    public void closeWindow(Button closeButton) throws IOException{
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
+
+    public void minimizeWindow(Button closeButton) throws IOException{
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.setIconified(true);
     }
 
     public record SceneController(Scene scene, Controller controller){}
